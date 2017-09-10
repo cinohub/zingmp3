0be00a3b3e290bd044355143006ddf3d67d462af
DuongKK <duongnv1996@gmail.com>
2017-09-11 03:25:50 +0700

    add source

---
+++ download.php
@@ -0,0 +1,51 @@
+<?php
+if(isset($_POST['submit']))
+{
+ $link = $_POST['submit'];
+ $token = gettoken();
+ if ($token != "") 
+ {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $link);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 1);
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, '?fromvn=true&requestdata={"token":"'.$token.'"}');
+    $page = curl_exec($ch);
+    echo  curl_getinfo($ch, CURLINFO_EFFECTIVE_URL);
+    curl_close($ch);
+}else{
+    echo "error token";
+}
+}
+
+function gettoken()
+{
+    $acc = array(
+        array("zid.kaiaozonpecazinih447","gaochanhkieu.vn"),
+        array("gocmobile.pro","gocmobile123")
+        );
+    
+    $dem = count($acc);
+    $ngaunhien = rand(0,$dem-1);
+    $user = $acc[$ngaunhien][0];
+    $pass = md5($acc[$ngaunhien][1]);
+    
+    $linklist = 'http://api.mp3.zing.vn/api/mobile/auth/usertoken?requestdata={"user":"'.$user.'","pwd":"'.$pass.'"}';
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $linklist);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER,false);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST,false);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    $page = curl_exec($ch);
+    curl_close($ch);
+
+    $infotoken = json_decode($page);
+    $token = $infotoken->token;
+
+    return $token;
+}
---
+++ getlink.php
@@ -0,0 +1,31 @@
+<?php
+if(isset($_POST['submit']))
+{
+    $url      = $_POST['submit'];
+    $temp     = explode("/", $url);
+    $idbaihat = str_replace(".html","",$temp[count($temp)-1]);
+    if ($idbaihat != "") 
+    {
+      
+           $linklist = 'http://api.mp3.zing.vn/api/mobile/song/getsonginfo?requestdata={"id":"'.$idbaihat.'"}';
+
+           $ch = curl_init();
+           curl_setopt($ch, CURLOPT_URL, $linklist);
+           curl_setopt($ch, CURLOPT_SSL_VERIFYPEER,false);
+           curl_setopt($ch, CURLOPT_SSL_VERIFYHOST,false);
+           curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+           curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+           $page = curl_exec($ch);
+           curl_close($ch);
+
+           $data = json_decode($page);
+    // echo chuyenlink($data->link_download->{"lossless"});
+           echo $page;
+    }
+    else
+    {
+        echo "Lỗi cmnr: Không tìm thấy ID bài hát! Link phải có dạng: http://mp3.zing.vn/bai-hat/Ghen-Khac-Hung-ERIK-MIN/ZW7FC0I7.html";
+    }
+}
+
